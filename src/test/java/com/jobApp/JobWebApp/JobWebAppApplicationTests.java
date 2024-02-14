3307ea964d2c1fde040ff7283149aa9f521ff4af
git2923 <springboot2923@gmail.com>
2024-02-14 11:16:33 +0530

    initial commit

---
+++ src/test/java/com/jobApp/JobWebApp/JobWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobApp.JobWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
